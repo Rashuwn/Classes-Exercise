66541b497a1fc29d405846dad8bc4a135e033327
Rashuwn Brown <rashuwnxbrown@gmail.com>
2025-07-07 13:28:02 -0500

    Car Exercise

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "Volkswagen";
+            carOne.Model = "Jetta";
+            carOne.Year = 2022;
+
+            Console.WriteLine($"This is a {carOne.Make}, {carOne.Model}, made in the year {carOne.Year}.");
         }
     }
 }
